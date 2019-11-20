a33c6a3a2abd2ba25adc87f270eac86c8d43382e
Craig Patik <craig@patik.com>
2019-11-20 09:40:58 +0100

    check start time more aggressively

--- src/StartTime.js
+++ src/StartTime.js
@@ -16,16 +16,23 @@ class StartTime extends React.Component {
         updateTime(val)
     }
 
+    // The time input doesn't fire as many change events as we'd like,
+    // so manually check it whenever there's action on its container
+    onCheckTime = () => {
+        this.updateTime(document.getElementById('startTime').value)
+    }
+
     render() {
         const { time } = this.props
 
         return (
-            <form className="form-inline">
+            <form className="form-inline" onKeyUp={this.onCheckTime} onClick={this.onCheckTime}>
                 <StyledLabel htmlFor="startTime">Start time:</StyledLabel>
                 <StyledInput
                     type="time"
                     className="form-control form-control-inline"
                     value={time}
+                    id="startTime"
                     onChange={evt => this.updateTime(evt.target.value)}
                 />
             </form>
