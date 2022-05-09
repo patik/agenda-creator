ee28b8a720998515aef30e0be7b115e7a9573ca8
Craig Patik <craig@patik.com>
2022-05-09 09:40:37 +0200

    initial commit

---
+++ src/index.d.ts
@@ -0,0 +1,32 @@
+// declare module 'ga'
+
+import { Moment } from 'moment'
+
+type Slot = {
+    id: number
+    time: number
+    desc: string
+}
+
+type Result = {
+    text: string
+    key: string
+    // For Confluence output:
+    begin: Moment
+    end: Moment
+    desc: string
+    time: number
+}
+
+type Prefs = { duration: boolean; time: boolean }
+
+type State = {
+    slots: Slot[]
+    start: string
+    elapsed: number
+    results: any[]
+    prefs: {
+        time: true
+        duration: false
+    }
+}
